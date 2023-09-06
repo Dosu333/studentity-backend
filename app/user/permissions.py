0abef2b9ce9258e28c5c0c4039099777070628e2
Dosu333 <vladamir1865@gmail.com>
2023-09-06 14:25:03 +0000

    add admin permissions

--- app/user/permissions.py
+++ app/user/permissions.py
@@ -10,6 +10,14 @@ class IsSuperAdmin(permissions.BasePermission):
         return bool(request.user and request.user.roles and 'SUPERADMIN' in request.user.roles)
 
 
+class IsAdmin(permissions.BasePermission):
+    """Allows access only to admin users. """
+    message = "Only Admins are authorized to perform this action."
+
+    def has_permission(self, request, view):
+        return bool(request.user and request.user.roles and 'ADMIN' in request.user.roles)
+
+
 class IsEmployer(permissions.BasePermission):
     """Allows access only to admin users. """
     message = "Only Admins are authorized to perform this action."
