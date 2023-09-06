f45532e36fb5afe9217bfc94520d2671dc5498d8
Dosu333 <vladamir1865@gmail.com>
2023-09-06 14:35:03 +0000

    remove s3

--- app/core/storage_backends.py
+++ app/core/storage_backends.py
@@ -1,19 +1,14 @@
-from storages.backends.s3boto3 import S3Boto3Storage
 from storages.backends.azure_storage import AzureStorage
 import os
 
 class AzureMediaStorage(AzureStorage):
-    account_name = 'studentitystatic' # Must be replaced by your <storage_account_name>
-    account_key = os.environ.get('AZURE_STORAGE_KEY') # Must be replaced by your <storage_account_key>
+    account_name = 'studentitystatic'
+    account_key = os.environ.get('AZURE_STORAGE_KEY')
     azure_container = 'media'
     expiration_secs = None
 
 class AzureStaticStorage(AzureStorage):
-    account_name = 'studentitystatic' # Must be replaced by your storage_account_name
-    account_key = os.environ.get('AZURE_STORAGE_KEY') # Must be replaced by your <storage_account_key>
+    account_name = 'studentitystatic' 
+    account_key = os.environ.get('AZURE_STORAGE_KEY') 
     azure_container = 'static'
     expiration_secs = None
-
-class MediaStorage(S3Boto3Storage):
-    location = 'media'
-    file_overwrite = False
